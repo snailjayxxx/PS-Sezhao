67184f4944b71ea40e4fcf296bde052dbc9186fd
snailjayxxx <42602200+snailjayxxx@users.noreply.github.com>
2026-08-04 15:27:11 +0900

    新增Lightroom Classic插件清单

---
+++ lightroom-classic/PS-Sezhao.lrplugin/Info.lua
@@ -0,0 +1,16 @@
+return {
+    LrSdkVersion = 15.0,
+    LrSdkMinimumVersion = 15.4,
+    LrToolkitIdentifier = 'com.snailjoss.pssezhao.lightroom',
+    LrPluginName = 'PS-Sezhao 胶片去色罩',
+    VERSION = { major = 0, minor = 3, revision = 0, build = 0 },
+
+    LrLibraryMenuItems = {
+        {
+            title = 'PS-Sezhao：转正所选负片',
+            file = 'ProcessSelected.lua',
+        },
+    },
+
+    LrPluginInfoProvider = 'PluginInfoProvider.lua',
+}
