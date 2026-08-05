9aaedd36a689a03e26d3395055104ebb60ce26a9
snailjayxxx <42602200+snailjayxxx@users.noreply.github.com>
2026-08-05 14:55:49 +0900

    同步Lightroom插件版本至v0.5.5

--- lightroom-classic/PS-Sezhao.lrplugin/Info.lua
+++ lightroom-classic/PS-Sezhao.lrplugin/Info.lua
@@ -3,7 +3,7 @@ return {
     LrSdkMinimumVersion = 15.4,
     LrToolkitIdentifier = 'com.snailjoss.pssezhao.lightroom',
     LrPluginName = 'PS-Sezhao 胶片去色罩',
-    VERSION = { major = 0, minor = 5, revision = 4, build = 0 },
+    VERSION = { major = 0, minor = 5, revision = 5, build = 0 },
 
     LrLibraryMenuItems = {
         {
