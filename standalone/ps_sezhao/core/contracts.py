332ab7b6b5f627c2664c580e22bff278c6629ffc
snailjayxxx <42602200+snailjayxxx@users.noreply.github.com>
2026-08-05 20:40:02 +0900

    提升几何与项目数据合同版本

--- standalone/ps_sezhao/core/contracts.py
+++ standalone/ps_sezhao/core/contracts.py
@@ -7,7 +7,8 @@ MATH_CONTRACT_VERSION = 1
 RAW_DECODE_CONTRACT_VERSION = 1
 PROXY_CONTRACT_VERSION = 1
 OUTPUT_QUEUE_CONTRACT_VERSION = 1
-PROJECT_SCHEMA_VERSION = 2
+GEOMETRY_CONTRACT_VERSION = 1
+PROJECT_SCHEMA_VERSION = 3
 
 
 @dataclass(frozen=True)
@@ -16,6 +17,7 @@ class ProcessingContract:
     raw_decode_version: int = RAW_DECODE_CONTRACT_VERSION
     proxy_version: int = PROXY_CONTRACT_VERSION
     output_queue_version: int = OUTPUT_QUEUE_CONTRACT_VERSION
+    geometry_version: int = GEOMETRY_CONTRACT_VERSION
     project_schema_version: int = PROJECT_SCHEMA_VERSION
 
 
